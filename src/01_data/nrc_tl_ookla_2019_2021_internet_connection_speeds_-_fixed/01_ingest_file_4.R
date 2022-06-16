2c00dcbb7c9d64e859e7e59677eb09d62d7bea39
Aaron Schroeder <dads2busy@gmail.com>
2022-06-16 14:28:20 -0400

    rm test files

--- src/01_data/nrc_tl_ookla_2019_2021_internet_connection_speeds_-_fixed/01_ingest_file_4.R
+++
@@ -1,9 +0,0 @@
-# dataset creation code - data source ingest - file 4
-# source file: s3://ookla-open-data/shapefiles/performance/type=fixed/year=2022/quarter=1/2022-01-01_performance_fixed_tiles.zip
-
-# Import source file and save to original for backup
-source_file <- "s3://ookla-open-data/shapefiles/performance/type=fixed/year=2022/quarter=1/2022-01-01_performance_fixed_tiles.zip"aws.s3::save_object(
-	object = "s3://ookla-open-data/shapefiles/performance/type=fixed/year=2022/quarter=1/2022-01-01_performance_fixed_tiles.zip"
-	region = ""
-	file = "data/nrc_tl_ookla_2019_2021_internet_connection_speeds_-_fixed/original/2022-01-01_performance_fixed_tiles.zip"
-)
--- src/01_data/nrc_tl_ookla_2019_2021_internet_connection_speeds_-_fixed/01_ingest_file_8.R
+++
@@ -1,9 +0,0 @@
-# dataset creation code - data source ingest - file 8
-# source file: s3://ookla-open-data/shapefiles/performance/type=fixed/year=2019/quarter=1/2019-07-01_performance_fixed_tiles.zip
-
-# Import source file and save to original for backup
-source_file <- "s3://ookla-open-data/shapefiles/performance/type=fixed/year=2019/quarter=1/2019-07-01_performance_fixed_tiles.zip"aws.s3::save_object(
-	object = "s3://ookla-open-data/shapefiles/performance/type=fixed/year=2019/quarter=1/2019-07-01_performance_fixed_tiles.zip"
-	region = ""
-	file = "data/nrc_tl_ookla_2019_2021_internet_connection_speeds_-_fixed/original/2019-07-01_performance_fixed_tiles.zip"
-)
